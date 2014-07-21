34006217c1cf5eaf3875f3974edb21e9e2b6c67f
Brian Clements <brian@clementscode.com>
2014-07-20 22:23:08 -0500

    ditched datastore
    just load the data from static files
    added datatype for indexing

---
+++ starmap/stardata.go
@@ -0,0 +1,101 @@
+package starmap
+
+import (
+    "sort"
+    "geom"
+    "strconv"
+    "os"
+    "strings"
+    "bufio"
+)
+
+type Star struct {
+    /* may be blank */
+    Name string
+    /* Hipparchos start catalog number */
+    HipNum int32
+    /* more negative is brighter */
+    Magnitude float64
+    /* geohash of right declination and ascension */
+    GeoHash string
+}
+
+type Stardata []*Star
+
+/* sort interface */
+func (sd Stardata) Len() int {
+    return len(sd)
+}
+
+/* sort interface */
+func (sd Stardata) Swap(i, j int) {
+    sd[i], sd[j] = sd[j], sd[i]
+}
+
+/* sort interface */
+func (sd Stardata) Less(i, j int) bool {
+    return sd[i].GeoHash < sd[j].GeoHash
+}
+
+/* takes in a geohash key, returns index into data slice
+see sort.Search() for more details */
+func (sd Stardata) FindIndex(geohash string) int {
+    return sort.Search(len(sd), func(i int) bool {
+        return sd[i].GeoHash >= geohash
+    })
+}
+
+/* returns the star that has a matching geohash or nil if not found */
+func (sd Stardata) Find(geohash string) *Star {
+    i := sd.FindIndex(geohash)
+    if i < len(sd) && sd[i].GeoHash == geohash {
+        return sd[i]
+    } else {
+        return nil
+    }
+}
+
+/* takes in two geohash search keys
+returns slice of data that is between those two keys */
+func (sd Stardata) Range(start, end string) Stardata {
+    startIndex := sd.FindIndex(start)
+    endIndex := sd.FindIndex(end)
+    return sd[startIndex:endIndex]
+}
+
+func LoadData(datafile string) (Stardata, error) {
+    f, err := os.Open(datafile)
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+    scanner := bufio.NewScanner(f)
+    rval := make(Stardata, 0, 32)
+    for scanner.Scan() {
+        star := new(Star)
+        line := scanner.Text()
+        parts := strings.Split(line, "\t")
+        if len(parts) != 6 {
+            continue
+        }
+        //id, idErr := strconv.ParseInt(parts[0], 10, 32)
+        hip, hipErr := strconv.ParseInt(parts[1], 10, 32)
+        if hipErr != nil {
+            hip = 0
+        }
+        star.HipNum = int32(hip)
+        star.Name = parts[2]
+        ra, raErr := strconv.ParseFloat(parts[3], 64)
+        dec, decErr := strconv.ParseFloat(parts[4],64)
+        mag, magErr := strconv.ParseFloat(parts[5], 64)
+        if raErr != nil || decErr != nil || magErr != nil {
+            continue
+        }
+        star.Magnitude = mag
+        coord := geom.NewPoint2D(ra, dec)
+        star.GeoHash = coord.GeoHash(geom.STELLAR)
+        rval = append(rval, star)
+    }
+    sort.Sort(rval)
+    return rval, nil
+}
--- starmap/starmap.go
+++ starmap/starmap.go
@@ -7,27 +7,10 @@ import (
     "geom"
 	"render"
 	"render/style"
-    "strconv"
-    "os"
-    "strings"
-    "bufio"
-    "appengine"
-    "appengine/datastore"
     "math"
 )
 
 const StarType string = "Star"
-
-type Star struct {
-    /* may be blank */
-    Name string
-    /* Hipparchos start catalog number */
-    HipNum int32
-    /* more negative is brighter */
-    Magnitude float64
-    /* geohash of right declination and ascension */
-    GeoHash string
-}
 
 func init() {
 	/* handler() defined below */
@@ -44,76 +27,15 @@ func decToGrid(dec float64) int {
     return int(math.Floor(300 - scaled))
 }
 
-func loadData(c appengine.Context) error {
-    f, err := os.Open("data/bright.tsv")
-    if err != nil {
-        return err
-    }
-    defer f.Close()
-    scanner := bufio.NewScanner(f)
-    starBuff := make([]*Star, 2048)
-    keyBuff := make([]*datastore.Key, 2048)
-    index := 0
-    for scanner.Scan() {
-        star := new(Star)
-        line := scanner.Text()
-        parts := strings.Split(line, "\t")
-        if len(parts) != 6 {
-            continue
-        }
-        id, idErr := strconv.ParseInt(parts[0], 10, 32)
-        hip, hipErr := strconv.ParseInt(parts[1], 10, 32)
-        if hipErr != nil {
-            hip = 0
-        }
-        star.HipNum = int32(hip)
-        star.Name = parts[2]
-        ra, raErr := strconv.ParseFloat(parts[3], 64)
-        dec, decErr := strconv.ParseFloat(parts[4],64)
-        mag, magErr := strconv.ParseFloat(parts[5], 64)
-        if idErr != nil || raErr != nil || decErr != nil || magErr != nil {
-            continue
-        }
-        star.Magnitude = mag
-        coord := geom.NewPoint2D(ra, dec)
-        star.GeoHash = coord.GeoHash(geom.STELLAR)
-        key := datastore.NewKey(c, StarType, "", id, nil)
-        starBuff[index] = star
-        keyBuff[index] = key
-        index += 1
-        if index >= len(starBuff) {
-            _, err = datastore.PutMulti(c, keyBuff, starBuff)
-            if err != nil {
-                return err
-            }
-            index = 0
-        }
-    }
-    if index > 0 {
-        starBuff = starBuff[:index+1]
-        keyBuff = keyBuff[:index+1]
-        _, err = datastore.PutMulti(c, keyBuff, starBuff)
-        if err != nil {
-            return err
-        }
-    }
-    return nil
-}
 
 func doErr(w http.ResponseWriter, err error) {
     http.Error(w, err.Error(), http.StatusInternalServerError)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    q := datastore.NewQuery(StarType)
-    count, err := q.Count(c)
-    if count < 1000 {
-        err = loadData(c)
-        if err != nil {
-            doErr(w, err)
-            return
-        }
+    data, err := LoadData("data/bright.tsv")
+    if err != nil {
+        doErr(w, err)
     }
 	smlCircle := style.NewPointStyle(1, color.White, style.CIRCLE)
 	midCircle := style.NewPointStyle(2, color.White, style.CIRCLE)
@@ -125,18 +47,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
     lowerHash, upperHash := geom.BBoxHash(min, max, geom.STELLAR)
     trans := geom.CreateTransform(min, max, width, height, geom.STELLAR)
 	img := render.Create(width, height, color.Black)
-    q = datastore.NewQuery(StarType).
-        Filter("GeoHash >=", lowerHash).Filter("GeoHash <", upperHash)
-    for t:= q.Run(c); ; {
-        var s Star
-        _, err = t.Next(&s)
-        if err == datastore.Done {
-            break
-        }
-        if err != nil {
-            doErr(w, err)
-            return
-        }
+    stars := data.Range(lowerHash, upperHash)
+    for _, s := range(stars) {
         coord, err := geom.UnHash(s.GeoHash, geom.STELLAR)
         if err != nil {
             doErr(w, err)
