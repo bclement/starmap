655209b48c977358d180a747a63eb42d0df1ed07
Brian Clements <brian@clementscode.com>
2014-07-21 22:44:24 -0500

    added basic url parsing for wms

--- starmap/starmap.go
+++ starmap/starmap.go
@@ -8,6 +8,8 @@ import (
 	"render"
 	"render/style"
     "math"
+    "strconv"
+    "strings"
 )
 
 const StarType string = "Star"
@@ -27,9 +29,52 @@ func decToGrid(dec float64) int {
     return int(math.Floor(300 - scaled))
 }
 
-
 func doErr(w http.ResponseWriter, err error) {
     http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
+/* parse integer url parameter
+return defaultValue if parameter isn't present or is malformed */
+func intParam(key string, defaultValue int, r *http.Request) int {
+    value := r.FormValue(key)
+    rval := defaultValue
+    if value != "" {
+        tmp, err := strconv.ParseInt(value, 10, 32)
+        if err == nil {
+            rval = int(tmp)
+        }
+    }
+    return rval
+}
+
+/* parse float from string value
+return defaultValue if value is malformed */
+func parseFloat(value string, defaultValue float64) float64 {
+    rval, err := strconv.ParseFloat(value, 64)
+    if err != nil {
+        rval = defaultValue
+    }
+    return rval
+}
+
+/* parse bounding box url parameter
+return full bounds if parameter isn't present or is malformed */
+func parseBbox(key string, r *http.Request) (*geom.Point, *geom.Point) {
+    minx := 0.0
+    miny := -90.0
+    maxx := 24.0
+    maxy := 90.0
+    value := r.FormValue(key)
+    if value != "" {
+        parts := strings.Split(value, ",")
+        if len(parts) == 4 {
+            minx = parseFloat(parts[0], minx)
+            miny = parseFloat(parts[1], miny)
+            maxx = parseFloat(parts[2], maxx)
+            maxy = parseFloat(parts[3], maxy)
+        }
+    }
+    return geom.NewPoint2D(minx, miny), geom.NewPoint2D(maxx, maxy)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -40,10 +85,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	smlCircle := style.NewPointStyle(1, color.White, style.CIRCLE)
 	midCircle := style.NewPointStyle(2, color.White, style.CIRCLE)
 	lrgCircle1 := style.NewPointStyle(3, color.White, style.CIRCLE)
-    width := 1024
-    height := 512
-    min := geom.NewPoint2D(0, -90)
-    max := geom.NewPoint2D(24, 90)
+    width := intParam("width", 1024, r)
+    height := intParam("height", 512, r)
+    min, max := parseBbox("bbox", r)
     lowerHash, upperHash := geom.BBoxHash(min, max, geom.STELLAR)
     trans := geom.CreateTransform(min, max, width, height, geom.STELLAR)
 	img := render.Create(width, height, color.Black)
